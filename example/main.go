0480ef12294eccb55ed6ab09545fae8d312705f1
sungo <sungo@sungo.us>
2023-05-13 00:21:11 -0400

    example now forces requests into multiple batches

--- example/main.go
+++ example/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"git.sr.ht/~sungo/dataloader"
 )
@@ -9,6 +11,7 @@ import (
 type result string
 
 func fetch(keys []string) (map[string]result, error) {
+	fmt.Printf("Batch: %d records requested\n", len(keys))
 	results := make(map[string]result)
 	for idx := range keys {
 		key := keys[idx]
@@ -23,10 +26,27 @@ func main() {
 		panic(err)
 	}
 
-	result, err := loader.Load("wat")
-	if err != nil {
-		panic(err)
+	var (
+		wg sync.WaitGroup
+		i  = 0
+	)
+
+	for i < 10 {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			// Force things into multiple batches
+			time.Sleep(time.Duration(j) * time.Millisecond)
+			result, err := loader.Load(fmt.Sprintf("wat%d", j))
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("worker %d : %s\n", j, result)
+		}(i)
+
+		i++
 	}
 
-	fmt.Printf("%s\n", result)
+	wg.Wait()
 }
